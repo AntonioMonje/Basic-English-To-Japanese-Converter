dff15201c7adbeb0673b06265a28be2d6d133cb2
Antonio Monje <monje001@cougars.csusm.edu>
2017-12-20 20:04:36 -0800

    update file

--- LanguageConverter.py
+++ LanguageConverter.py
@@ -13,25 +13,19 @@ def EnglishToJapanese():
 
 def main():
     #translate = {'mother: 'okaa-san', 'brother': 'onii-chan'}
+    #create the dictionary
     translate = EnglishToJapanese()
-    keylist = translate.keys()
-    eng = 'mother'
-    for keys in keylist:
-        if keys == eng:
-            print(eng, ' translates to: ', translate[keys])
-        else:
-            print(eng, 'not found')
 
-
-
-
+    #get the user input for the word they want to translate
     eng = input('Enter word you want to translate to japanese: ')
     eng = eng.lower()
+    keylist = translate.keys()
     running = True
 
-
+    #while loop to key translating a word until you dont type yes
     while running:
         flag = False
+        #use a for loop to iterate through the dictionary to match the word and translate it
         for keys in keylist:
             if keys == eng:
                 print(eng, ' translates to: ', translate[keys])
@@ -40,11 +34,12 @@ def main():
 
         if flag == False:
             print(eng, ' not found could not translate sorry!')
+            print('\n')
 
+        #ask if they want to enter another word if yes get the user input again
         ans = input('Do you want translate another word enter Yes anything else will terminate the program: ')
         if ans == 'yes' or ans == 'YES' or ans == 'Yes':
             eng = input('Enter word you want to translate to japanese: ')
-
             eng = eng.lower()
             pass
         else:
